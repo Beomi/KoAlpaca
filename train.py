35759f40ea7550bda57fc9deff3201bf01797f1a
Junbum Lee <jun@beomi.net>
2023-03-17 11:40:08 +0000

    Add Korean dataset and Korean Promp

--- train.py
+++ train.py
@@ -31,14 +31,16 @@ DEFAULT_BOS_TOKEN = "</s>"
 DEFAULT_UNK_TOKEN = "</s>"
 PROMPT_DICT = {
     "prompt_input": (
-        "Below is an instruction that describes a task, paired with an input that provides further context. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+        "Below is an instruction that describes a task, paired with an input that provides further context.\n"
+        "아래는 작업을 설명하는 명령어와 추가적 맥락을 제공하는 입력이 짝을 이루는 예제입니다.\n\n"
+        "Write a response that appropriately completes the request.\n요청을 적절히 완료하는 응답을 작성하세요.\n\n"
+        "### Instruction(명령어):\n{instruction}\n\n### Input(입력):\n{input}\n\n### Response(응답):"
     ),
     "prompt_no_input": (
-        "Below is an instruction that describes a task. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Response:"
+        "Below is an instruction that describes a task.\n"
+        "아래는 작업을 설명하는 명령어입니다.\n\n"
+        "Write a response that appropriately completes the request.\n명령어에 따른 요청을 적절히 완료하는 응답을 작성하세요.\n\n"
+        "### Instruction(명령어):\n{instruction}\n\n### Response(응답):"
     ),
 }
 
