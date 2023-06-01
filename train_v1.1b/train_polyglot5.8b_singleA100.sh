5a94de70f6b94a71b05df91f9b709656e716e556
Junbum Lee <jun@beomi.net>
2023-06-01 18:45:16 +0900

    Update train_polyglot5.8b_singleA100.sh

--- train_v1.1b/train_polyglot5.8b_singleA100.sh
+++ train_v1.1b/train_polyglot5.8b_singleA100.sh
@@ -1,7 +1,7 @@
 # Works on A100 80G single GPU
 python run_clm.py \
 --model_name_or_path='EleutherAI/polyglot-ko-5.8b' \
---train_file='./KoAlpaca_v1.1.json' \
+--train_file='./KoAlpaca_v1.1a_textonly.json' \
 --num_train_epochs=3 \
 --block_size=2048 \
 --per_device_train_batch_size=2 \
