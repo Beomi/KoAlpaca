de3304243226c1c444b5dba859415e20224248c7
Junbum Lee <jun@beomi.net>
2023-06-01 18:45:34 +0900

    Update train.sh

--- train_v1.1b/train.sh
+++ train_v1.1b/train.sh
@@ -1,7 +1,7 @@
 # Works on A100 80G x4
 torchrun --nproc_per_node=4 --master_port=34321 run_clm.py \
 --model_name_or_path='EleutherAI/polyglot-ko-12.8b' \
---train_file='./KoAlpaca_v1.1.json' \
+--train_file='KoAlpaca_v1.1a_textonly.json' \
 --num_train_epochs=2 \
 --block_size=1024 \
 --per_device_train_batch_size=1 \
