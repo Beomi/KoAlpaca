bc9a958e7e80b3d2a7d30a79bdc615fd9999442f
Junbum Lee <jun@beomi.net>
2023-06-02 21:04:33 +0900

    Create train_KoRWKV_H100.sh

---
+++ train_v1.1b/train_KoRWKV_H100.sh
@@ -0,0 +1,17 @@
+python run_clm.py \
+--model_name_or_path='beomi/KoRWKV-6B' \
+--train_file='KoAlpaca_v1.1a_textonly.json' \
+--num_train_epochs=1 \
+--block_size=1024 \
+--per_device_train_batch_size=1 \
+--gradient_accumulation_steps=8 \
+--torch_dtype=float32 \
+--fp16 \
+--output_dir='KoRWKV-6B-koalpaca-v1.1a' \
+--do_train \
+--optim='adafactor' \
+--learning_rate='2e-5' \
+--logging_strategy='steps' \
+--logging_first_step \
+--run_name='KoRWKV-6B-koalpaca-v1.1a' \
+--low_cpu_mem_usage --overwrite_output_dir
